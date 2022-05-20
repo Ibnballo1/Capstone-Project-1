3a19ba57f7c37613daab9fa2a492984ecf098e99
Ibnballo1 <belloabdullateef035@gmail.com>
2022-05-21 00:22:32 +0100

    Fix javascript errors

--- main.js
+++ main.js
@@ -8,73 +8,67 @@ const hiddenScrol = document.getElementById('hideOverflow');
 let isDisplay = true;
 
 menuBar.addEventListener('click', () => {
-    if (isDisplay) {
-      hamburgerBtn.style.display = 'none';
-      times.style.display = 'block';
-      closeBar.classList.add('icon-menu');
-      lists.classList.replace('navis', 'js-nav-list');
-      hiddenScrol.classList.add('hide-scroll');
-      isDisplay = false;
-    } else {
-      hamburgerBtn.style.display = 'block';
-      times.style.display = 'none';
-      closeBar.classList.replace('icon-menu', 'nav-menu');
-      lists.classList.replace('js-nav-list', 'navis');
-      hiddenScrol.classList.remove('hide-scroll');
-      isDisplay = true;
-    }
-  });
+  if (isDisplay) {
+    hamburgerBtn.style.display = 'none';
+    times.style.display = 'block';
+    closeBar.classList.add('icon-menu');
+    lists.classList.replace('navis', 'js-nav-list');
+    hiddenScrol.classList.add('hide-scroll');
+    isDisplay = false;
+  } else {
+    hamburgerBtn.style.display = 'block';
+    times.style.display = 'none';
+    closeBar.classList.replace('icon-menu', 'nav-menu');
+    lists.classList.replace('js-nav-list', 'navis');
+    hiddenScrol.classList.remove('hide-scroll');
+    isDisplay = true;
+  }
+});
 
-  // Feautre Speaker
-  const data = [
-    {
-      title: 'Yochai Benkler',
-      description: 'Berkman Professor of Entrepreneural Legal Studies at Harvard Law School',
-      linkImg: 'images/speaker_01.png',
-      //techs: ['Html', 'CSS', 'Javascript'],
-      more: 'Benkler Studies commoms-based peer production, and puublished his seminar book The Wealth of Networks in 2006.'
-    },
-    {
-      title: 'Kilnam Chon',
-      description: 'Director of Art Center Nabi and a board member Webtekhy',
-      linkImg: 'images/speaker_02.png',
-      //techs: ['Html', 'CSS', 'Javascript'],
-      more: `As the main venue for new media art production in Nigeria. Webtekhy promotes 
-      cross-disciplinary collaboration and understanding among science technologies, humanities, and arts.`
-    },
-    {
-      title: 'SohYeong Noh',
-      description: 'Director of Art Center Nabi and a board member Webtekhy',
-      linkImg: 'images/speaker_03.png',
-      //techs: ['Html', 'CSS', 'Javascript'],
-      more: `As the main venue for new media art production in Nigeria. Webtekhy promotes cross-disciplinary
-       collaboration and understanding among science technologies, humanities, and arts.`
-    },
-    {
-      title: 'Julia Leda',
-      description: 'Berkman Professor of Entrepreneural Legal Studies at Harvard Law School',
-      linkImg: 'images/speaker_04.png',
-      //techs: ['Html', 'CSS', 'Javascript'],
-      more: `Benkler Studies commoms-based peer production, and puublished his seminar book The Wealth of Networks in 2006.`
-    },
-    {
-        title: 'Lilia Tretikov',
-        description: 'Lilia Tretikov Professor of Entrepreneural Legal Studies at Harvard Law School',
-        linkImg: 'images/speaker_05.png',
-        //techs: ['Html', 'CSS', 'Javascript'],
-        more: `As the main venue for new media art production in Nigeria. Webtekhy promotes cross-disciplinary
-         collaboration and understanding among science technologies, humanities, and arts.`
-      },
-      {
-        title: 'Ryan Merkley',
-        description: 'Ryan Merkley Professor of Entrepreneural Legal Studies at Harvard Law School',
-        linkImg: 'images/speaker_06.png',
-        //techs: ['Html', 'CSS', 'Javascript'],
-        more: `Benkler Studies commoms-based peer production, and puublished his seminar book The Wealth of Networks in 2006.`
-      },
-  ];
+// Feautre Speaker
+const data = [
+  {
+    title: 'Yochai Benkler',
+    description: 'Berkman Professor of Entrepreneural Legal Studies at Harvard Law School',
+    linkImg: 'images/speaker_01.png',
+    more: 'Benkler Studies commoms-based peer production, and puublished his seminar book The Wealth of Networks in 2006.',
+  },
+  {
+    title: 'Kilnam Chon',
+    description: 'Director of Art Center Nabi and a board member Webtekhy',
+    linkImg: 'images/speaker_02.png',
+    more: `As the main venue for new media art production in Nigeria. Webtekhy promotes 
+      cross-disciplinary collaboration and understanding among science technologies, humanities, and arts.`,
+  },
+  {
+    title: 'SohYeong Noh',
+    description: 'Director of Art Center Nabi and a board member Webtekhy',
+    linkImg: 'images/speaker_03.png',
+    more: `As the main venue for new media art production in Nigeria. Webtekhy promotes cross-disciplinary
+       collaboration and understanding among science technologies, humanities, and arts.`,
+  },
+  {
+    title: 'Julia Leda',
+    description: 'Berkman Professor of Entrepreneural Legal Studies at Harvard Law School',
+    linkImg: 'images/speaker_04.png',
+    more: `Benkler Studies commoms-based peer production, and puublished his seminar book The Wealth of Networks in 2006.`,
+  },
+  {
+    title: 'Lilia Tretikov',
+    description: 'Lilia Tretikov Professor of Entrepreneural Legal Studies at Harvard Law School',
+    linkImg: 'images/speaker_05.png',
+    more: `As the main venue for new media art production in Nigeria. Webtekhy promotes cross-disciplinary
+         collaboration and understanding among science technologies, humanities, and arts.`,
+  },
+  {
+    title: 'Ryan Merkley',
+    description: 'Ryan Merkley Professor of Entrepreneural Legal Studies at Harvard Law School',
+    linkImg: 'images/speaker_06.png',
+    more: 'Benkler Studies commoms-based peer production, and puublished his seminar book The Wealth of Networks in 2006.',
+  },
+];
 
-  // Creating the feature speaker section dynamically
+// Creating the feature speaker section dynamically
 const featureSpeakerSection = document.querySelector('.speakers-details');
 let counter = 0;
 
